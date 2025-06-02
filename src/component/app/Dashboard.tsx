d065fee3efa039f48ca5fe302073bf7bf63afa1a
bholusaini <bholusaini686@gmail.com>
2025-06-02 14:42:26 +0530

    setup chart.js  add barChar add pie chart

--- src/component/app/Dashboard.tsx
+++ src/component/app/Dashboard.tsx
@@ -1,6 +1,121 @@
 import Card from "../shared/card"
 import schoollogo from "../../assets/school.png"
+import Chart from 'chart.js/auto'
+import { useEffect, useRef } from "react"
 const Dashboard = () => {
+
+const BarChartRef = useRef<HTMLCanvasElement | null>(null);
+const PieChartRef = useRef<HTMLCanvasElement | null>(null);
+const BarChartInstanceRef = useRef<Chart | null>(null)
+const PieChartInstanceRef = useRef<Chart | null>(null)
+
+useEffect(()=>{
+
+  BarChartInstanceRef.current = barChart();
+  PieChartInstanceRef.current= pieChart()
+
+  return ()=>{
+
+    if(BarChartInstanceRef.current){
+      BarChartInstanceRef.current.destroy()
+      BarChartInstanceRef.current=null
+    }
+    if(PieChartInstanceRef.current){
+      PieChartInstanceRef.current.destroy()
+      PieChartInstanceRef.current=null
+    }
+  }
+
+},[])
+
+  const barChart = (): Chart| null => {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    const canvas = BarChartRef.current;
+    if (!canvas) 
+      return null;
+
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) 
+      return null;
+
+    if(BarChartInstanceRef.current)
+      BarChartInstanceRef.current.destroy()
+
+    return new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count),
+            backgroundColor: '#3b82f6',
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: true,
+          }
+        }
+      }
+    });
+  };
+
+  const pieChart = (): Chart | null => {
+
+  const canvas = PieChartRef.current;
+  if (!canvas) 
+    return null;
+
+  const ctx = canvas.getContext("2d");
+  if (!ctx) 
+    return null;
+  if(PieChartInstanceRef.current)
+      PieChartInstanceRef.current.destroy()
+
+  return new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    },
+
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        }
+      }
+    }
+  });
+  
+ 
+  return null;
+};
+
   return (
     <> 
   <div className="p-4">
@@ -59,6 +174,18 @@ const Dashboard = () => {
         </div>    
 
 
+      </div>
+    </Card>
+  </div>
+
+  <div className="p-4">
+    <Card>
+      <div className="grid grid-cols-2 items-center gap-2 w-full ">
+
+ <div className="flex flex-col gap -2 w-full h-full  justify-center items-center"> <h1 className="text-2xl font-semibold text-black">Admission Overview</h1>  <canvas ref={BarChartRef}></canvas></div>
+ <div  className="flex flex-col gap -2 w-full h-96 justify-center items-center">  
+  <h1 className="text-2xl font-semibold text-black">Payment Status</h1>  <canvas ref={PieChartRef}></canvas></div>   
+    
       </div>
     </Card>
   </div>
