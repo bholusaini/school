59132062d0d2f932733ee4c1e1e0460f2c9aed15
bholusaini <bholusaini686@gmail.com>
2025-06-03 16:35:57 +0530

    setup createsubject and fetch subject using httprequest on frontend

--- src/component/app/Subject.tsx
+++ src/component/app/Subject.tsx
@@ -1,21 +1,23 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Button from "../shared/button"
 import Card from "../shared/card"
 import Drawer from "../shared/drawer"
 import IconButton from "../shared/iconButton"
 import Form, { type FormDataType } from "../shared/form"
 import Input from "../shared/input"
-import { useLocation } from "react-router-dom"
+import {  useLocation } from "react-router-dom"
 import schoolLogo from "../../assets/school.png"
 import HttpInterceptor from "../../lib/HttpInterceptor"
 import { toast } from "react-toastify"
 import CatchError from "../../lib/CatchError"
-import axios from "axios"
+
+
 
 const Subject = () => {
     const [open,setOpen] =useState(false)
     const {pathname} = useLocation()
+    const [subject,setSubject] = useState<any[]>([])
 
     const getPathname = (path:string)=>{
            const fristPath = path.split("/").pop()          
@@ -25,7 +27,7 @@ const Subject = () => {
       const createSubject = async(values:FormDataType)=>{
             try{
                 console.log("hi");
-                const {data} = await axios.post("http://localhost:8080/subject", values)        
+                const {data} = await HttpInterceptor.post("/subject", values)        
                 console.log(data);        
                 toast.success(data.message);
             }
@@ -33,7 +35,24 @@ const Subject = () => {
             {
               CatchError(err)
             }
+    }
+
+    useEffect(()=>{
+        fetchSubject()
+    },[])
+
+    const fetchSubject = async ( )=>{
+
+        try{
+            const  {data} = await HttpInterceptor.get("/subject")
+            setSubject(data)
+          
         }
+        catch(err:unknown)
+        {
+            CatchError(err)
+        }
+    }
 
 
   return (
@@ -72,12 +91,12 @@ const Subject = () => {
           <Form className="grid grid-cols-2 gap-4" onValue={createSubject}>           
             <div className="flex flex-col gap-2 "> 
                 <label >Subject's Name</label>
-                <Input name="subjectname" type="text" placeholder=" Enter Subject Name"/>
+                <Input name="subjectName" type="text" placeholder=" Enter Subject Name"/>
             </div>
 
             <div className="flex flex-col gap-2 "> 
                 <label >Full Mark's</label>
-                <Input name="fullmark" type="number" placeholder="90"/>
+                <Input name="fullMark" type="number" placeholder="90"/>
             </div>          
             
             <div className="w-fit"><Button type="danger"> Add Now</Button></div>           
@@ -87,145 +106,28 @@ const Subject = () => {
         <div className="p-4"> 
             <Card>
                 <div className="grid grid-cols-4 gap-8 ">
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
+
+                {
+                   subject &&  subject.map((item,index)=>(
+                           <Card key={index}>
+                        <div  className="flex flex-col items-center gap-2">
                             <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
                             <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
+                                <h1 className="text-2xl font-semibold text-black">{item.subjectName}</h1>
+                                <h3>{item.fullMark}</h3>
                             </div>
                             <div className="flex justify-center items-center gap-4">
                                 <IconButton type="success" icon="edit-line"/>
                                 <IconButton type="danger" icon="delete-bin-line"/>
                             </div>
                         </div>
-                    </Card>                 
+                    </Card>   
+                    ))
+                }
+
+                               
                     
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
-                    
-                    <Card>
-                        <div className="flex flex-col items-center gap-2">
-                            <div className=" p-4 rounded-b-md  text-6xl font-bold"><i className="ri-book-3-fill"></i></div>
-                            <div className="flex flex-col justify-center items-center gap-2">
-                                <h1 className="text-2xl font-semibold text-black">Maths</h1>
-                                <h3>100 Marks</h3>
-                            </div>
-                            <div className="flex justify-center items-center gap-4">
-                                <IconButton type="success" icon="edit-line"/>
-                                <IconButton type="danger" icon="delete-bin-line"/>
-                            </div>
-                        </div>
-                    </Card>                 
+                 
                     
                 </div>
             </Card>
